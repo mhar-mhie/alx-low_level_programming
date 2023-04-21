4669e8351455504c3e47dd4610e2205d162e7627
mhar-mhie <adwoam813@gmail.com>
2023-04-21 16:04:31 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,17 @@
  */
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        int i;
+	int n = 97;
+	int m = 65;
 
-	for (i = 0; i < 52; i++)
+	while (n <= 122)
 	{
-		putchar(alp[i]);
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
 	}
 	putchar('\n');
 	return (0);
