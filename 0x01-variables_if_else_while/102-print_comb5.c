cda4ce18b7531f28f2c44a82f70e9b07e37849a0
mhar-mhie <adwoam813@gmail.com>
2023-04-21 19:07:31 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,9 +14,9 @@ int main(void)
 	for (b = a + 1; b <= 99; b++)
 	{
 	putchar((a / 10) + '0');
-	putchar((b % 10) + '0');
+	putchar((a % 10) + '0');
 	putchar(' ');
-	putchar((a / 10) + '0');
+	putchar((b / 10) + '0');
 	putchar((b % 10) + '0');
 	if (a == 98 && b == 99)
 	continue;
