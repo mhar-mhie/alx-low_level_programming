91a841dbf0b6812833aefc054268fef0f2c270c4
mhar-mhie <adwoam813@gmail.com>
2023-04-26 13:14:17 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_alphabet - Entry point
+ * Return:0
  */
 void print_alphabet(void)
 {
